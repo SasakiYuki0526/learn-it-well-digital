7a817c12f0d3935b1497e0b1b23226421eacecfa
yuki <10756044@ntub.edu.tw>
2022-03-05 15:22:40 +0800

    make it better

--- core/views.py
+++ core/views.py
@@ -5,11 +5,20 @@ def index(request):
     return render(request, 'index.html')
 
 
-def login(request):
-    return render(request, 'login.html')
+def about(request):
+    return render(request, 'about.html')
 
-def register(request):
-    return render(request, 'register.html')
+def contact(request):
+    return render(request, 'contact.html')
 
-def ForgetPwd(request):
-    return render(request, 'ForgetPwd.html')
+def project(request):
+    return render(request, 'project.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def testimonial(request):
+    return render(request, 'testimonial.html')
