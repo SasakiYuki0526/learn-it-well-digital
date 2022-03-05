5a0c473a491da2d6582099ac02eb5424e4d84a23
yuki <10756044@ntub.edu.tw>
2022-03-05 14:45:44 +0800

    make it better

--- core/views.py
+++ core/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 
-def homepage(request):
-    return render(request, 'homepage.html')
+def index(request):
+    return render(request, 'index.html')
 
 
 def login(request):
